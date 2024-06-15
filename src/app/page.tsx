85be9a690d8b7daa8e72a3fcd880bbd802ba0322
mael098 <juanvillegasmorales@outlook.com>
2024-06-15 11:11:05 -0600

    jajajaj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,46 +2,12 @@ import { redirect } from "next/navigation";
 import { prisma } from "@/db";
 
 export default async function Home() {
-  try {
-    const labs = await prisma.labs.findMany();
+  const labs = await prisma.labs.findMany({})
+  redirect('/labs/' + ( labs[0]?.id??'null' ))
 
-    if (labs.length === 0) {
-      return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          No hay laboratorios disponibles.
-        </main>
-      );
-    }
-    redirect('/labs/' + labs[0].id);
-  } catch (error) {
-    console.error("Error fetching labs:", error);
-
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Error al cargar los laboratorios.
-      </main>
-    );
-  }
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      esta pagina no debe existir
+    </main>
+  );
 }
-
-
-
-
-
-
-
-
-
-// import { redirect } from "next/navigation";
-// import { prisma } from "@/db";
-
-// export default async function Home() {
-//   const labs = await prisma.labs.findMany({})
-//   redirect('/labs/' + ( labs[0]?.id??'null' ))
-
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       esta pagina no debe existir
-//     </main>
-//   );
-// }
