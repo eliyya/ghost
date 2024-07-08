06d216d243981a6cf1f07396219e94568ff3c7a9
eliyya <tecno_eli@hotmail.com>
2024-07-08 16:01:45 -0600

    db

---
+++ prisma/migrations/20240708215614_remove_subject/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the `subjects` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "subjects";
+PRAGMA foreign_keys=on;
