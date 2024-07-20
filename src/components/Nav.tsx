c3da84cc41adec56cca31adc9b4d014f1fd3333f
eliyya <tecno_eli@hotmail.com>
2024-07-20 17:51:52 -0600

    nav centrado

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,8 +11,8 @@ export async function Nav(props: NavProps) {
     const labs = await prisma.labs.findMany({})
     
     return (
-        <div className="flex justify-between items-center align-middle w-full px-2 py-5 bg-[#333]">
-            <div className="flex gap-4 items-center">
+        <nav className="flex justify-between items-center align-middle w-full px-2 py-5 bg-[#333]">
+            <div className="flex gap-4 flex-1 items-center">
                 <Link href={"/"}>
                     <Image src={"/logo.jpg"} alt={"IISE-Logo"} height={10} width={50} />
                 </Link>
@@ -25,6 +25,6 @@ export async function Nav(props: NavProps) {
             </div>
             {props.labs.length === 1 && <div className="text-white">{props.labs[0].name}</div>}
             {props.labs.length > 1 && <NavOptions labs={props.labs} />}
-            <Link className="flex justify-center justify-items-center text" href={"/user"}>👤</Link>
-        </div>)
+            <Link className="flex flex-1 justify-end justify-items-center text" href={"/user"}>👤</Link>
+        </nav>)
 }
