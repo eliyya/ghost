abfc0a7efaee190891a9c00b913facfb6ec9aee3
eliyya <tecno_eli@hotmail.com>
2024-08-24 11:15:30 -0600

    remove null lab

--- src/app/labs/page.tsx
+++ src/app/labs/page.tsx
@@ -1,13 +1,47 @@
 import { redirect } from 'next/navigation'
 import { prisma } from '@/lib/db'
+import { Nav } from '@/components/Nav'
+import { getPosibleUser } from '@/lib/auth'
+import { ButtonPrimaryLink } from '@/components/Buttons'
 
 export default async function LabsPage() {
     const labs = await prisma.laboratory.findMany({
         select: { name: true },
     })
-    if (!labs.length) redirect('/labs/null')
     if (labs.length === 1)
         redirect(`/labs/${labs[0].name.replace(/\s/g, '-').toLowerCase()}`)
+    const user = await getPosibleUser()
 
-    return <div>Si puedes imaginarlo puedes programarlo</div>
+    return (
+        <>
+            <Nav
+                isAdmin={user?.admin}
+                labs={[{ id: '', name: 'Laboratorios', active: true }]}
+            />
+            <main className="flex gap-1 flex-1 flex-col align-middle text-center justify-center item-center">
+                <h1 className="text-3xl">Oh no...</h1>
+                <p className="text-xl">
+                    Parece que no existen laboratorios registrados en el sistema
+                </p>
+                {user?.admin ? (
+                    <>
+                        <p>
+                            Para hacer uso de este sistema debe haber al menos 1
+                            laboratorio registrado
+                        </p>
+                        <div className="flex justify-center m-3">
+                            <ButtonPrimaryLink href="/admin/labs/new">
+                                Registrar Laboratorio
+                            </ButtonPrimaryLink>
+                        </div>
+                    </>
+                ) : (
+                    <p>
+                        Porfavor pidele a un administrador que registre uno
+                        antes de volver aqui
+                    </p>
+                )}
+            </main>
+        </>
+    )
 }
