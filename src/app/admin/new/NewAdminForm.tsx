64f9b6b1ea3786d3aebb6db23511f21161d72da0
eliyya <tecno_eli@hotmail.com>
2024-08-24 14:04:19 -0600

    validate new admin form

--- src/app/admin/new/NewAdminForm.tsx
+++ src/app/admin/new/NewAdminForm.tsx
@@ -1,54 +1,76 @@
 'use client'
 
-import { Input, SubmitPrimaryInput } from "@/components/Input"
-import { useEffect, useState } from "react";
+import { Input, SubmitPrimaryInput } from '@/components/Input'
+import { parseName } from '@/lib/utils'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export interface NewAdminFormSubmitFunction {
-    (props: {
-        name: string;
-        username: string;
-        password: string;
-    }): Promise<{ status: 'error' | 'succes', message: string }>;
+    (props: { name: string; username: string; password: string }): Promise<{
+        status: 'error' | 'succes'
+        message: string
+    }>
 }
 export interface NewAdminFormProps {
     submit: NewAdminFormSubmitFunction
 }
 export function NewAdminForm(props: NewAdminFormProps) {
-    const [username, setUsername] = useState < string > ("")
-    const [password, setPassword] = useState < string > ("")
-    const [password2, setPassword2] = useState < string > ("")
-    const [passwordError, setPasswordError] = useState < string > ("")
-    const [password2Error, setPassword2Error] = useState < string > ("")
+    const [name, setName] = useState<string>('')
+    const [username, setUsername] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [password2, setPassword2] = useState<string>('')
+    const [passwordError, setPasswordError] = useState<string>('')
+    const [password2Error, setPassword2Error] = useState<string>('')
+    const router = useRouter()
 
-    const submit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        const target = event.target as typeof event.target & { name: { value: string } }
-        if (!password.match(/[A-Z]/g)) return setPasswordError("La contrasenia debe tener al menos una mayuscula")
-        if (!password.match(/[a-z]/g)) return setPasswordError("La contrasenia debe tener al menos una minuscula")
-        if (!password.match(/[0-9]/g)) return setPasswordError("La contrasenia debe tener al menos un numero")
-        if (!password.match(/[!?_\-+=*&%$#]/g)) return setPasswordError('La contrasenia debe tener al menos un caracter especial')
-        if (password.length < 8) return setPasswordError("La contrasenia debe tener al menos 8 caracteres")
-        if (password.match(/[^A-Za-z0-9!?_\-+=*&%$#]/g)) return setPasswordError("La contrasenia solo puede tener los siguientes caracteres especiales: !?_-=+*&%$#")
+    const submit = async () => {
+        if (!password.match(/[A-Z]/g))
+            return setPasswordError(
+                'La contrasenia debe tener al menos una mayuscula',
+            )
+        if (!password.match(/[a-z]/g))
+            return setPasswordError(
+                'La contrasenia debe tener al menos una minuscula',
+            )
+        if (!password.match(/[0-9]/g))
+            return setPasswordError(
+                'La contrasenia debe tener al menos un numero',
+            )
+        if (!password.match(/[!?_\-+=*&%$#]/g))
+            return setPasswordError(
+                'La contrasenia debe tener al menos un caracter especial',
+            )
+        if (password.length < 8)
+            return setPasswordError(
+                'La contrasenia debe tener al menos 8 caracteres',
+            )
+        if (password.match(/[^A-Za-z0-9!?_\-+=*&%$#]/g))
+            return setPasswordError(
+                'La contrasenia solo puede tener los siguientes caracteres especiales: !?_-=+*&%$#',
+            )
         // check password
-        if (password != password2) return setPassword2Error("Las contrasenias no coinciden")
+        if (password != password2)
+            return setPassword2Error('Las contrasenias no coinciden')
         // check username
         const response = await props.submit({
-            name: target.name.value,
+            name: name,
             username: username,
             password: password,
         })
-        window.location.replace("/admin/docentes")
+        router.push('/admin/docentes')
     }
 
     return (
         <form
             className="w-72 p-4 border border-black rounded-lg flex flex-col"
-            onSubmit={submit}
+            action={submit}
         >
             <Input
-                type='text'
-                placeholder='Nombre del Administrador'
+                type="text"
+                placeholder="Nombre del Administrador"
                 name="name"
+                value={name}
+                onChange={e => setName(parseName(e.target.value))}
             />
             <Input
                 type="text"
@@ -56,28 +78,32 @@ export function NewAdminForm(props: NewAdminFormProps) {
                 placeholder="Usuario Clave"
                 prefix="@"
                 value={username}
-                onChange={e => setUsername(e.target.value.toLowerCase())}
+                onChange={e =>
+                    setUsername(
+                        e.target.value.toLowerCase().replace(/[@ \!\\\/]/g, ''),
+                    )
+                }
             />
             <Input
-                type='password'
+                type="password"
                 placeholder="Constraseña"
                 name="password"
                 value={password}
                 error={passwordError}
-                onChange={(e) => {
+                onChange={e => {
                     setPassword(e.target.value)
-                    setPasswordError("")
+                    setPasswordError('')
                 }}
             />
             <Input
-                type='password'
+                type="password"
                 placeholder="Confirmar Contraseña"
                 name="password2"
                 value={password2}
                 error={password2Error}
-                onChange={(e) => {
+                onChange={e => {
                     setPassword2(e.target.value)
-                    setPassword2Error("")
+                    setPassword2Error('')
                 }}
             />
             <SubmitPrimaryInput value="Registrar"></SubmitPrimaryInput>
--- src/app/admin/new/page.tsx
+++ src/app/admin/new/page.tsx
@@ -24,6 +24,10 @@ export default async function NewAdminPage() {
             <h1 className="text-3xl text-center py-8">
                 Crea una cuenta de administrador
             </h1>
+            <p className="text-xl text-center py-8">
+                Para continuar con la configuracion de esta aplicacion se
+                necesita crear una cuenta administrador
+            </p>
             <main className="flex flex-col flex-1 justify-center items-center">
                 <NewAdminForm submit={add} />
             </main>
