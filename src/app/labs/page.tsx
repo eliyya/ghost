bf45a4988f071c7711648f038a534fce05fe42ec
eliyya <tecno_eli@hotmail.com>
2024-08-24 11:10:44 -0600

    find labs by name

--- src/app/labs/page.tsx
+++ src/app/labs/page.tsx
@@ -1,14 +1,12 @@
-import { redirect } from "next/navigation";
-import { prisma } from "@/lib/db";
+import { redirect } from 'next/navigation'
+import { prisma } from '@/lib/db'
 
 export default async function LabsPage() {
-    const labs = await prisma.laboratory.findMany({});
-    if (!labs.length) redirect("/labs/null");
-    if (labs.length === 1) redirect(`/labs/${labs[0].id}`);
+    const labs = await prisma.laboratory.findMany({
+        select: { name: true },
+    })
+    if (!labs.length) redirect('/labs/null')
+    if (labs.length === 1) redirect(`/labs/${labs[0].name.replace(/\s/g, '-')}`)
 
-    return (
-        <div>
-            Si puedes imaginarlo puedes programarlo
-        </div>
-    );
+    return <div>Si puedes imaginarlo puedes programarlo</div>
 }
